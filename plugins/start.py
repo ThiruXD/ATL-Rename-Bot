9b4c97e8f9d4a93a7388f8d96e40d1f43d9dd294
ThiruXD ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2024-01-05 08:05:55 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -37,8 +37,7 @@ async def start(client, message):
     old = insert(int(message.chat.id))
     try:
         id_one = message.text.split(' ')[1]
-    except IndexError:
-        id_one = None
+    except:
         txt = f"""**ʜᴇʟʟᴏ - {message.from_user.mention} , \nɪ  ᴀᴍ  ᴀɴ  ᴀᴅᴠᴀɴᴄᴇ  ꜰɪʟᴇ  ʀᴇɴᴀᴍᴇʀ  ᴀɴᴅ  ᴄᴏɴᴠᴇʀᴛᴇʀ  ʙᴏᴛ  ᴡɪᴛʜ  ᴘᴇʀᴍᴀɴᴇɴᴛ  ᴀɴᴅ  ᴄᴜsᴛᴏᴍ  ᴛʜᴜᴍʙɴᴀɪʟ  sᴜᴘᴘᴏʀᴛ. \n\nᴊᴜsᴛ  sᴇɴᴅ  ᴍᴇ  ᴀɴʏ  ᴠɪᴅᴇᴏ  ᴏʀ ᴅᴏᴄᴜᴍᴇɴᴛ !!**"""
         if LAZY_PIC:
             await message.reply_photo(photo=LAZY_PIC, caption=txt, reply_markup=InlineKeyboardMarkup(
